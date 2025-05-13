46ddb77892175450068ff7761f179685d10c5950
abde7 <abebechehitegmail.com>
2025-05-13 07:39:40 -0930

    etch-a-sketch game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const reset=document.querySelector("#reset")
 const container=document.querySelector("#container");
 const noof=document.querySelector("#noof");
 function getrandomInt(max){
@@ -13,19 +13,51 @@ function getRandomColor(){
 }
 
 
+
+function numberOfsqure(){
+ squreNumber=prompt("how many squre do you want?",16)
+ if (isNaN(squreNumber)||squreNumber>100||squreNumber<=0) {
+   alert("give me the number between 0-100")
+  
+ }
+ else{
+    
 function createGrid(squreNumber){
 const noofsqure=squreNumber**2;
-const size =960/squreNumber;
+const size =850/squreNumber;
 for (let i = 0; i < noofsqure; i++) {
   const grid =document.createElement("div");
   grid.setAttribute("class", "pixel")
-
+    
     grid.style.width=size+"px"
    grid.style.height=size+"px"
-
+    
    grid.addEventListener("mouseover",()=>{
-    grid.style.backgroundColor = getRandomColor()   
-   })
+    grid.style.opacity=0.5;
+    
+    
+            grid.style.backgroundColor = getRandomColor()   
+  
+    
+    })
     container.appendChild(grid);
-} }
-createGrid(5);
+
+
+      reset.addEventListener("click",()=>{
+  grid.style.backgroundColor="white"
+})
+} 
+
+}
+createGrid(squreNumber);
+
+ }
+
+
+}
+
+
+
+
+   
+
